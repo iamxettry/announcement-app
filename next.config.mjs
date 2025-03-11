aec7c896241d192349835d7392ce1c462087d083
TheSubMish <subodhmishra015@gmail.com>
2025-03-11 18:41:21 +0545

    fix: image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns:[
-          {
-              // protocol: 'https',
-              hostname: '127.0.0.1',
-          },
-        ]
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "api.announcemate.thesubmish.me",
       },
+      {
+        protocol: "https",
+        hostname: "api.announcemate.thesubmish.me",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
